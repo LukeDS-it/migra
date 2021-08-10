2e8b17e84c72c101107c00b96f08a2e384f43d9c
LukeDS-it <luca.mcgyver@gmail.com>
2021-08-10 17:32:12 +0200

    App configuration for standalone or server modes

--- src/main/scala/it/ldsoftware/starling/StarlingApp.scala
+++ src/main/scala/it/ldsoftware/starling/StarlingApp.scala
@@ -1,3 +1,20 @@
 package it.ldsoftware.starling
 
-object StarlingApp extends App {}
+import com.typesafe.config.ConfigFactory
+import it.ldsoftware.starling.configuration.AppConfig
+
+object StarlingApp extends App {
+
+  val config = AppConfig(ConfigFactory.load())
+
+  config.getMode match {
+    case AppConfig.ServerMode     => processServer(config)
+    case AppConfig.StandaloneMode => processStandalone(config, args)
+    case _                        => throw new Error("STARLING_MODE env var not found!")
+  }
+
+  def processServer(config: AppConfig): Unit = {}
+
+  def processStandalone(config: AppConfig, args: Array[String]): Unit = {}
+
+}
--- src/main/scala/it/ldsoftware/starling/configuration/AppConfig.scala
+++ src/main/scala/it/ldsoftware/starling/configuration/AppConfig.scala
@@ -2,4 +2,14 @@ package it.ldsoftware.starling.configuration
 
 import com.typesafe.config.Config
 
-case class AppConfig(private val config: Config)
+case class AppConfig(private val config: Config) {
+
+  def getMode: String = config.getString("it.ldsoftware.starling.mode").toLowerCase
+
+}
+
+object AppConfig {
+  val StandaloneMode = "standalone"
+  val ServerMode = "server"
+}
+
